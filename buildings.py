ad38f498e28286bb1926b4f5bf75227132b8d8a4
Brian Mount <bvmount@gmail.com>
2012-12-03 00:16:39 -0800

    instead of commenting out

---
+++ buildings.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import json, psycopg2
+fout = open('parcelcollection.json', 'w')
+conn = psycopg2.connect(database='buildingfootprints')
+cur = conn.cursor()
+
+
+ctr = [-122.410312, 37.761012]
+
+w = .003
+
+x0 = ctr[0]-w
+y0 = ctr[1]-w
+x1 = ctr[0]+w
+y1 = ctr[1]+w
+
+#cur.execute("""select *, st_asgeojson(st_transform(sfootprint, 4326)) from
+cur.execute("""select *, st_asgeojson(sfootprint) from
+sbldgs where sfootprint &&
+st_transform(st_envelope(st_geomfromtext('LINESTRING(%s %s,
+%s %s)', 4326)), 900913)""", (x0,y0,x1,y1))
+rv = cur.fetchall()
+f = {"type":"FeatureCollection", "features":[]}
+for i in rv:
+    f['features'].append({"geometry":json.loads(i[2]), "properties":{"height":float(i[1])}})
+
+fout = open('parcelcollection.json', 'w')
+fout.write(json.dumps(f))
+
+
